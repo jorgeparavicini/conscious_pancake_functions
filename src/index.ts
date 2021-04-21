b478f102a7aa907c3ba229ec50dfd48c53c98bfd
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-04-21 12:41:37 +0200

    Remove default image

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,10 @@ exports.createProfile = functions.auth.user().onCreate((user) => {
     firstName: "",
     lastName: "",
     username: user.displayName ?? user.email ?? "Anonymous",
-    profilePictureName: uuidv4(),
+    profilePictureName: null,
   };
-
-  const bucket = admin.storage().bucket();
-
-  const fileUpload = bucket.file("outline_face_black_48dp.png")
-      .copy(user.uid + "/images/profile_picture.jpg");
-  const userUpload = admin
+  return admin
       .firestore()
       .doc("users/" + user.uid)
       .set(userObject);
-
-  return Promise.all([fileUpload, userUpload]);
 });
-
-/**
- * Creates a UUID
- * @return {string} the create UUID
- */
-function uuidv4(): string {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    const r = Math.random() * 16 | 0; const v = c == "x" ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
